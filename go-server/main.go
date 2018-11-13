f5deacce5b068365270d25fc0fa4cdef01568e1f
周先生 <Mr.Zhou@zhouxianshengdeMacBook-Pro.local>
2018-11-13 13:16:40 +0800

    优化获取运行地址逻辑，添加了测试环境的接口验证调试

--- go-server/main.go
+++ go-server/main.go
@@ -9,42 +9,34 @@ import (
 	"net"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"path/filepath"
+	"os"
 )
 
 const (
-	port = ":50051"
+	port = ":9001"
 )
 
 type server struct{}
 
-var config map[string]string
+var alismsConfig map[string]string
 
 func (s *server) Send(ctx context.Context, in *pb.Request) (*pb.Reply, error) {
 	var defaultTemplate string
 	if (in.Template != "") {
 		defaultTemplate = in.Template;
 	} else {
-		defaultTemplate = config["defaultTemplate"];
+		defaultTemplate = alismsConfig["defaultTemplate"];
 	}
-	error := alisms.InitConfig("http://dysmsapi.aliyuncs.com", config["accessKeyId"], config["accessKeySecret"], config["signName"]).Send(in.Mobile, in.Params, defaultTemplate)
+	error := alisms.InitConfig("http://dysmsapi.aliyuncs.com", alismsConfig["accessKeyId"], alismsConfig["accessKeySecret"], alismsConfig["signName"]).Send(in.Mobile, in.Params, defaultTemplate)
 	if error != nil {
 		return nil, error
 	}
 	return &pb.Reply{Code: 200, Msg: "发送成功"}, nil
 }
 
-// 加载配置文件
-func loadConfig(field string) {
-	cfg, err := goconfig.LoadConfigFile("/Users/Mr.Zhou/Project/golang/grpc/go-server/config.ini")
-	if err != nil {
-		log.Println("读取配置文件失败[config.ini]")
-	}
-	value, _ := cfg.GetSection(field)
-	config = value
-}
-
 func main() {
-	loadConfig("alisms")
+	alismsConfig = loadConfig("alisms")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -55,4 +47,25 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+//获取当前运行路径，用于取配置文件
+func getDir() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir
+}
+
+// 加载配置文件
+// 这里如果是临时运行要注意写死地址，程序临时运行后会生成临时目录去运行，故而取不到config.ini
+// 编译后的程序就是正常的
+func loadConfig(field string) map[string]string {
+	cfg, err := goconfig.LoadConfigFile(getDir() + "/config.ini")
+	if err != nil {
+		log.Println("读取配置文件失败[config.ini]")
+	}
+	value, _ := cfg.GetSection(field)
+	return value
 }
--- php-client/demo.php
+++ php-client/demo.php
@@ -10,13 +10,16 @@ defined('APP_DIR') || define('APP_DIR', dirname(__FILE__));
 require_once APP_DIR . '/vendor/autoload.php';
 
 try {
-    $client = new \Sms\SmsClient("127.0.0.1:50051", [
+    //测试环境的grpc调用服务
+    //$client = new \Sms\SmsClient("www.fastgoo.net:9001", [
+    $client = new \Sms\SmsClient("127.0.0.1:9001", [
         'credentials' => Grpc\ChannelCredentials::createInsecure()
     ]);
     $request = new \Sms\Request();
     $request->setParams(json_encode(['code' => '123456']));
     $request->setMobile(15600087538);
     //$request->setTemplate("123");
+
     list($reply, $error) = $client->Send($request)->wait();
     if ($error->code) {
         exit("发送失败，错误信息：" . $error->details);
